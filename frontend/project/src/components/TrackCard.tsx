0e40a22b10f0b8ebb5c2dc6ba29a2c4fc014263b
amir.khaksar <khaksaramirmohmmad@gmail.com>
2026-06-11 10:39:45 +0330

    fix click trackcard button

--- frontend/project/src/components/TrackCard.tsx
+++ frontend/project/src/components/TrackCard.tsx
@@ -1,12 +1,23 @@
 import type { Track } from "../types/media";
 import { Play } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import { usePlayer } from "../contexts/playerContext";
 interface Props {
     track: Track;
 }
 
 const TrackCard = ({ track }: Props) => {
     const navigate = useNavigate();
+    const { track: currentTrack, setTrack, playToggle } = usePlayer();
+
+    const handlePlayClick = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        if (currentTrack?.id !== track.id) {
+            setTrack(track);
+        } else {
+            playToggle();
+        }
+    };
 
     return (
         <div
@@ -19,7 +30,10 @@ const TrackCard = ({ track }: Props) => {
                     alt={track.title}
                     className="rounded-xl mb-4 w-full"
                 />
-                <button className="absolute bottom-4 right-4 bg-emerald-500 w-10 h-10 rounded-full flex items-center justify-center opacity-0 group-hover:opacity-100 translate-y-3 group-hover:translate-y-0 transition cursor-pointer">
+                <button
+                    onClick={handlePlayClick}
+                    className="absolute bottom-4 right-4 bg-emerald-500 w-10 h-10 rounded-full flex items-center justify-center opacity-0 group-hover:opacity-100 translate-y-3 group-hover:translate-y-0 transition cursor-pointer"
+                >
                     <Play size={16} />
                 </button>
             </div>
