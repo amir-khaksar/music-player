19acc797429ef29352150b73c0a7ea9d8978cd19
amir.khaksar <khaksaramirmohmmad@gmail.com>
2026-06-12 09:10:28 +0330

    fix searchGrid h2

--- frontend/project/src/components/SearchGrid.tsx
+++ frontend/project/src/components/SearchGrid.tsx
@@ -1,15 +1,21 @@
 import type { Track } from "../types/media";
 import TrackCard from "./TrackCard";
+import { useLocation } from "react-router-dom";
 
 interface Props {
     tracks: Track[];
 }
 
 const SearchGrid = ({ tracks }: Props) => {
+    const location = useLocation();
     return (
         <div className="space-y-12">
             <section>
-                <h2 className="text-xl font-bold mb-6">Tracks</h2>
+                <h2 className="text-xl font-bold mb-6">
+                    {location.pathname === "/discover/liked"
+                        ? "Liked Tracks"
+                        : "Tracks"}
+                </h2>
                 <div className="grid gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
                     {tracks.map((track) => (
                         <TrackCard key={track.id} track={track} />
