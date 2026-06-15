c3031da6f1e86f950caf456b48c606ad9bdce95c
amir.khaksar <khaksaramirmohmmad@gmail.com>
2026-06-15 15:19:00 +0330

    fix like bug

--- frontend/project/src/pages/songDetail/index.tsx
+++ frontend/project/src/pages/songDetail/index.tsx
@@ -44,7 +44,7 @@ export default function SongDetail() {
 
     const clickHandler = () => {
         setLike((prev) => !prev);
-        if (isLiked) {
+        if (like) {
             unlikeSong(song.id);
         } else {
             likeSong(song.id);
